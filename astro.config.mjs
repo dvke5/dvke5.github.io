9f72ca379717c30b3045f067f87d3d2495812171
deltakiro <deltakilone@hotmail.com>
2025-08-11 02:21:57 +0530

    now?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dvke5.github.io/",
+  site: "https://dvke5.github.io",
   base: "/dvke5.github.io",
   // base: "",
 });
